102ca3a8d8c61db55d35b39eb502e7638e998fa4
metametadata <yuri.govor@gmail.com>
2016-01-19 00:12:55 +0200

    first version of gen-signals-add-and-toggle-todos generator

--- test/unit/todos.cljs
+++ test/unit/todos.cljs
@@ -39,19 +39,79 @@
   [app]
   (apply distinct? (map :id @(:todos (:view-model app)))))
 
-;;;;;;;;;;;;;;;;;;;;;;;; Generators
-; title is hardcoded, because we are not interested in testing different string values
-(def signals-add-todo
-  [[:on-update-field "some title"] :on-add])
+;;;;;;;;;;;;;;;;;;;;;;;; gen-signals-add-todos
+(defn signals-add-todos
+  [num]
+  ; title is hardcoded, because we are not interested in testing different string values
+  (->> (take num (repeat [[:on-update-field "some title"] :on-add]))
+       (apply concat)))
 
 (def gen-signals-add-todos
-  (->> gen/s-pos-int                                        ; the desired number of todos
-       (gen/fmap #(->> (take % (repeat signals-add-todo))
-                       (apply concat)))))
+  (gen/fmap signals-add-todos gen/s-pos-int))
+
+;;;;;;;;;;;;;;;;;;;;;;;; gen-signals-add-and-toggle-todos
+(defn with-ids
+  [adds-pattern]
+  (map #(-> {:adds          %1
+             :available-ids (range %2)})
+       adds-pattern
+       (reductions + adds-pattern)))
+
+(defn gen-toggle-pattern
+  [available-ids]
+  (gen/not-empty
+    (gen/vector
+      (gen/elements available-ids)
+      1 7)))
+
+(defn gen-with-toggle-patterns
+  "Generator which creates :toggle-pattern instead of every :available-ids."
+  [adds-with-ids]
+  (apply gen/tuple
+         (map (fn [pattern]
+                (gen/fmap #(-> pattern
+                               (dissoc :available-ids)
+                               (assoc :toggle-pattern %))
+                          (gen-toggle-pattern (:available-ids pattern))))
+              adds-with-ids)))
+
+(defn signals-toggle-todos
+  [pattern]
+  (map #(-> [:on-toggle %]) pattern))
+
+(defn signals-add-and-toggle-todos
+  [adds-with-toggles-pattern]
+  (mapcat #(concat (signals-add-todos (:adds %))
+                   (signals-toggle-todos (:toggle-pattern %)))
+          adds-with-toggles-pattern))
+
+; Generates a sequence of signals which adds and randomly toggles items
+(def gen-signals-add-and-toggle-todos
+  (let [gen-adds-pattern (gen/not-empty (gen/vector gen/s-pos-int))
+        ; e.g. generates: [1 3] <- first add 1 item, then add 3 items
+        gen-adds-pattern-with-ids (gen/fmap with-ids gen-adds-pattern)
+        ; becomes:
+        ; ({:adds 1, :available-ids (0)} <- id=0 is available after adding 1 item
+        ;  {:adds 3, :available-ids (0 1 2 3)}) <- after this step ids 0-3 are available
+        gen-adds-with-toggles-pattern (gen/bind gen-adds-pattern-with-ids gen-with-toggle-patterns)
+        ; e.g. generates:
+        ; ({:adds 1, :toggle-pattern [0 0]} <- toggle item 0 twice
+        ;  {:adds 3, :toggle-pattern [3 1 1 2]}) <- toggle 3, then 1, etc.
+        ]
+    (gen/fmap signals-add-and-toggle-todos gen-adds-with-toggles-pattern)
+    ; finally:
+    ;([:on-update-field "some title"] :on-add
+    ;  [:on-toggle 0] [:on-toggle 0]
+    ;  [:on-update-field "some title"] :on-add
+    ;  [:on-update-field "some title"] :on-add
+    ;  [:on-update-field "some title"] :on-add
+    ;  [:on-toggle 3] [:on-toggle 1] [:on-toggle 1] [:on-toggle 2])
+    ))
 
 #_(deftest debug
-  (doseq [signals (gen/sample gen-signals-add-todos 10)]
-    (println (count signals) (pr-str signals))))
+  (doseq [signals (gen/sample gen-signals-add-and-toggle-todos 6)]
+    ;(println (count signals))
+    (cljs.pprint/pprint signals)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;; Tests
 (deftest property-tests
@@ -66,6 +126,3 @@
             "self-test: number of created todos")
 
         (is (ids-unique? app))))))
-
-#_(deftest failing-unit-test
-    (is (= 5 (+ 2 2))))
