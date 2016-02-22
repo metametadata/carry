24992e7326f7e9a35f61cff92e1b70e037b82b42
metametadata <yuri.govor@gmail.com>
2016-02-22 02:17:40 +0200

    logging groups now are always closed

--- examples/todomvc/src/app/middleware/logging.cljs
+++ examples/todomvc/src/app/middleware/logging.cljs
@@ -7,12 +7,15 @@
   ([spec prefix]
    {:pre [(:control spec) (:reconcile spec)]}
    (-> spec
-       (update :control helpers/before-do
-               (fn [_model signal _dispatch]
-                 (.group js/console (str prefix "signal =") (pr-str signal))))
-       (update :control helpers/after-do
-               (fn [_model _signal _dispatch]
-                 (.groupEnd js/console)))
+       (update :control (fn wrap-control [control]
+                          (fn logged-control [model signal dispatch]
+                            (try
+                              (.group js/console (str prefix "signal =") (pr-str signal))
+                              (control model signal dispatch)
+
+                              ; this clause guarantees that group is closed even in case of excpetion
+                              (finally
+                                (.groupEnd js/console))))))
        (update :reconcile helpers/before-do
                (fn [_model action]
                  (println (str prefix "  action =") (pr-str action)))))))
