e9ab729c74a645703e27bfe6536be59139014c05
metametadata <yuri.govor@gmail.com>
2016-02-24 15:22:01 +0200

    cleaned lib project a bit

--- project.clj
+++ project.clj
@@ -3,75 +3,35 @@
                  [org.clojure/clojurescript "1.7.170"]
 
                  [reagent "0.5.1" :exclusions [cljsjs/react]]
-                 [cljsjs/react-with-addons "0.13.3-0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
-                 [com.rpl/specter "0.8.0"]
-                 [funcool/hodgepodge "0.1.4"]
-
-                 ; for tests:
-                 #_[org.clojure/test.check "0.9.0"]
-                 #_[com.gfredericks/test.chuck "0.2.5"]]
+                 [cljsjs/react-with-addons "0.13.3-0"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0"]
             [lein-doo "0.1.6"]]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "resources/private" "target"]
 
   :cljsbuild {
               :builds [#_{:id           "dev"
-                        :source-paths ["src"]
-                        :compiler     {:main                 core
-                                       :asset-path           "js/compiled/out"
-                                       :output-to            "resources/public/js/compiled/frontend.js"
-                                       :output-dir           "resources/public/js/compiled/out"
-                                       :source-map-timestamp true}
-                        :figwheel     {:on-jsload     "core/on-jsload"
-                                       :before-jsload "core/before-jsload"}}
+                          :source-paths ["src"]
+                          :compiler     {:main                 core
+                                         :asset-path           "js/compiled/out"
+                                         :output-to            "resources/public/js/compiled/frontend.js"
+                                         :output-dir           "resources/public/js/compiled/out"
+                                         :source-map-timestamp true}
+                          :figwheel     {:on-jsload     "core/on-jsload"
+                                         :before-jsload "core/before-jsload"}}
 
                        #_{:id           "min"
-                        :source-paths ["src"]
-                        :compiler     {:main          core
-                                       :output-to     "resources/public/js/compiled/frontend.js"
-                                       :optimizations :advanced
-                                       :pretty-print  false}}
+                          :source-paths ["src"]
+                          :compiler     {:main          core
+                                         :output-to     "resources/public/js/compiled/frontend.js"
+                                         :optimizations :advanced
+                                         :pretty-print  false}}
 
                        #_{:id           "test"
-                        :source-paths ["src/frontend" "test"]
-                        :compiler     {:main          'unit.runner
-                                       :output-to     "resources/private/js/compiled/testable.js"
-                                       :output-dir    "resources/private/js/compiled/out"
-                                       :optimizations :none
-                                       }}]}
-
-  :figwheel {
-             ;; :http-server-root "public" ;; default and assumes "resources" 
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1" 
-
-             :css-dirs ["resources/public/css"]             ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
-             })
+                          :source-paths ["src/frontend" "test"]
+                          :compiler     {:main          'unit.runner
+                                         :output-to     "resources/private/js/compiled/testable.js"
+                                         :output-dir    "resources/private/js/compiled/out"
+                                         :optimizations :none
+                                         }}]})
