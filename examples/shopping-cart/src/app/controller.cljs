a249e8dc399ee3cfc9b86f5a3bba5e53754708f9
metametadata <yuri.govor@gmail.com>
2016-03-02 01:04:09 +0200

    shopping-cart: more asserts

--- examples/shopping-cart/src/app/controller.cljs
+++ examples/shopping-cart/src/app/controller.cljs
@@ -7,7 +7,7 @@
   [shop]
   {:pre [(satisfies? api/ShopAPIProtocol shop)]}
   (fn control
-    [_model signal dispatch]
+    [model signal dispatch]
     (println "signal" signal)
     (match signal
            :on-get-all-products
@@ -16,10 +16,13 @@
                              #(dispatch :receive-products-error))
 
            [:on-add-to-cart id]
-           (dispatch [:add-to-cart id])
+           (do
+             (assert (not (:checking-out? model)))
+             (dispatch [:add-to-cart id]))
 
            :on-checkout
            (do
+             (assert (not (:checking-out? model)))
              (dispatch :checkout-request)
              (api/buy-products shop
                                "<orders data from model>"
