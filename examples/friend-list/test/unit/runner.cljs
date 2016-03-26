1f1cf8e2498584a3eda3ead4bba5cd232d6b158a
metametadata <yuri.govor@gmail.com>
2016-03-26 07:43:37 +0200

    added todos for routing behavior tests

--- examples/friend-list/test/unit/runner.cljs
+++ examples/friend-list/test/unit/runner.cljs
@@ -1,11 +1,13 @@
 (ns unit.runner
   (:require [cljs.test]
             [doo.runner :refer-macros [doo-tests]]
+            [unit.spec]
             [unit.controller]
             [unit.reconciler]
             [unit.view-model]))
 
 (doo-tests
+  'unit.spec
   'unit.controller
   'unit.reconciler
   'unit.view-model)
