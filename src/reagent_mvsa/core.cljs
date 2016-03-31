cebf7e94c3cfc70f0cdd5254227ed838dab3dcb7
metametadata <yuri.govor@gmail.com>
2016-03-31 13:58:37 +0300

    API change: now readonly model ratom is passed around instead of model reaction in order to allow users to call add-watch on it

--- src/reagent_mvsa/core.cljs
+++ src/reagent_mvsa/core.cljs
@@ -2,41 +2,55 @@
   (:require [reagent.core :as r])
   (:require-macros [reagent.ratom :refer [reaction]]))
 
+(defn -make-readonly!
+  "Prohibits using swap! and reset! on the specified atom. Returns the same atom."
+  [a]
+  (let [original-reset-fn (.bind (.-cljs$core$IReset$_reset_BANG_$arity$2 a) a)]
+    (specify! a
+      IReset
+      (-reset!
+        [this new-value]
+        (if (::force-reset? (meta new-value))
+          (original-reset-fn this new-value)
+          (assert nil (str "readonly atom cannot be reset to " (pr-str new-value))))))))
+
+(defn -force-reset!
+  "Bypasses write protection of the specified readonly atom."
+  [readonly-atom new-value]
+  (reset! readonly-atom (with-meta new-value {::force-reset? true})))
+
 (defn app
   "Constructs an app from a spec map with keys:
   :initial-model - Initial model value, must be a map.
 
-  :control - Function of args: [model-reaction signal dispatch-signal dispatch-action].
+  :control - Function of args: [model-readonly-ratom signal dispatch-signal dispatch-action].
   It performs side effects, can dispatch actions and new signals.
   By convention, it must be able to handle :on-start and :on-stop signals in order to be wrappable by middleware.
-  Model reaction is useful for reading actual model values in async code or even to dynamically subscribe to model changes.
+  Model ratom is useful for reading actual model values in async code and to subscribe to model changes.
+  Functions dispatch-signal and dispatch-action always return nil.
 
   :reconcile - Pure function of args: [model-value action]. Given an action, it must return the new model value.
 
   Returned app is a map with keys:
-  :model - A model reaction.
+  :model - A readonly model ratom.
 
   :dispatch-signal - Function with a single arg: a signal to be sent to an app. Returns nil.
-
-  :-model - Model ratom (exposed for debugging only).
 
   Data flow:
   -dispatch-signal-> (control) -dispatch-action-> (reconcile)"
   [{:keys [initial-model control reconcile] :as _spec}]
   {:pre [(map? initial-model) (fn? control) (fn? reconcile)]}
-  (let [model-ratom (r/atom initial-model)
-        model-reaction (reaction @model-ratom)]
-    (letfn [(dispatch-action [action] (swap! model-ratom reconcile action) nil)
-            (dispatch-signal [signal] (control model-reaction signal dispatch-signal dispatch-action) nil)]
-      {:model           model-reaction
-       :dispatch-signal dispatch-signal
-       :-model          model-ratom})))
+  (let [model-ratom (-make-readonly! (r/atom initial-model))]
+    (letfn [(dispatch-action [action] (-force-reset! model-ratom (reconcile @model-ratom action)) nil)
+            (dispatch-signal [signal] (control model-ratom signal dispatch-signal dispatch-action) nil)]
+      {:model           model-ratom
+       :dispatch-signal dispatch-signal})))
 
 (defn connect-ui
   "Arguments:
   app - the app for which UI should be created
-  view-model - function which, given a model reaction, returns Reagent reactions; returned value will be passed to a view:
-  view - Reagent compoment function with args: [view-model-return-value dispatch-signal]
+  view-model - function which, given a model ratom, returns reactions; returned value will be passed to a view:
+  view - Reagent component function with args: [view-model-return-value dispatch-signal]
 
   Note that view-model function will be called only once.
 
@@ -48,7 +62,7 @@
     [app-view-model app-view]))
 
 (defn track-keys
-  "Returns a map containing Reagent reactions to map entries specified by keys."
+  "Returns a map containing reactions to map entries specified by keys."
   [map-ratom keyseq]
   (into {}
         (for [key keyseq]
