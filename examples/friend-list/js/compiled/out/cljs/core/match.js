2e1f3d08fbe4b2aff8967dae8a2d84e32715ffd5
metametadata <yuri.govor@gmail.com>
2016-04-07 16:27:48 +0200

    Update documentation

--- examples/friend-list/js/compiled/out/cljs/core/match.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.7.228 {}
-goog.provide('cljs.core.match');
-goog.require('cljs.core');
-cljs.core.match.backtrack = (new Error());
-
-//# sourceMappingURL=match.js.map?rel=1460037774759
--- examples/friend-list/js/compiled/out/reagent/interop.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.7.228 {}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
-
-//# sourceMappingURL=interop.js.map?rel=1460037776529
