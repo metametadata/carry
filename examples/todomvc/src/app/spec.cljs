57b000cedbeb0c30ce9be60ec3558e62a84d37e5
metametadata <yuri.govor@gmail.com>
2016-04-15 00:19:35 +0300

    todomvc: make it friendly to Replay mode by ignoring loading from storage when mode is active

--- examples/todomvc/src/app/spec.cljs
+++ examples/todomvc/src/app/spec.cljs
@@ -6,6 +6,15 @@
             [middleware.history :as h]
             [middleware.persistence :as persistence]))
 
+(defn -wrap-load-from-storage
+  "Prohibits loading from storage in replay mode.
+  Without this hack replayed actions would make no effect since replaying happens before app loads model from storage."
+  [load-from-storage]
+  (fn wrapped-load-from-storage
+    [model loaded-model dispatch-signal]
+    (when (not (-> @model :middleware.devtools/debugger :replay-mode?))
+      (load-from-storage model loaded-model dispatch-signal))))
+
 (defn new-spec
   [history storage storage-key todo-titles]
   (-> {:initial-model (model/new-model todo-titles)
@@ -15,6 +24,8 @@
 
       ; Debugger deals with persistence itself, so we have to blacklist it here to get rid of loading conflicts.
       ; Token is blacklisted because on start we want the app to read current token from the urlbar instead of storage.
-      (persistence/add storage storage-key {:blacklist #{:middleware.devtools/debugger ::h/token}})
+      (persistence/add storage storage-key {:load-wrapper -wrap-load-from-storage
+                                            :blacklist    #{:middleware.devtools/debugger
+                                                            ::h/token}})
 
       (h/add history)))
