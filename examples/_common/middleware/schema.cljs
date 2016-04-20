2e175ca44666a8f4e6f4c1d2bb99ae75a167ca4b
metametadata <yuri.govor@gmail.com>
2016-04-20 20:53:16 +0300

    schema: more comments

--- examples/_common/middleware/schema.cljs
+++ examples/_common/middleware/schema.cljs
@@ -3,8 +3,10 @@
   (:require-macros [reagent.ratom :refer [run!]]))
 
 (defn add
-  "Middleware will validate the model returned from reconcile function.
-  Throws an exception if validation fails effectively prohibiting model change to unallowed state.
+  "Middleware will validate the model returned from reconcile function using Plumatic Schema library.
+  Strives to provide a better logging than s/defn.
+  Logs problems and throws an exception if validation fails effectively prohibiting model change to unallowed state.
+  
   Add it as close to spec as possible in order to not propagate the invalid model to other middlewares."
   [spec schema]
   (update spec :reconcile
@@ -12,7 +14,7 @@
             (fn validated-reconcile [model action]
               (let [new-model (reconcile model action)]
                 (when-let [problems (s/check schema new-model)]
-                  ; using console log for better printing in Chrome console
+                  ; using console log for better printing in Chrome console (in particular with binaryage/cljs-devtools)
                   (.log js/console "VALIDATION PROBLEMS:" problems)
                   (throw (ex-info "Model validation failed. See more info above." {})))
 
