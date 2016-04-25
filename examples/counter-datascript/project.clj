4f6d46387ecf696c03d21a01154c17baa8365894
metametadata <yuri.govor@gmail.com>
2016-04-25 23:41:27 +0300

    examples: removed commented code from project files

--- examples/counter-datascript/project.clj
+++ examples/counter-datascript/project.clj
@@ -36,36 +36,4 @@
                                        :optimizations  :advanced
                                        :pretty-print   false
                                        :compiler-stats true
-                                       :parallel-build false}}]}
-
-  :figwheel {
-             ;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
-
-             ;; :css-dirs ["resources/public/css"]             ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             })
+                                       :parallel-build false}}]})
--- examples/todomvc/project.clj
+++ examples/todomvc/project.clj
@@ -9,7 +9,7 @@
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [com.domkm/silk "0.1.2"]
 
-                 ; required by debugger:
+                 ; required by carry-devtools:
                  [com.rpl/specter "0.9.3"]
                  [funcool/hodgepodge "0.1.4"]
                  [prismatic/schema "1.1.0"]
@@ -57,37 +57,7 @@
                                        :compiler-stats true
                                        :parallel-build false}}]}
 
-  :figwheel {
-             ;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
-
-             :css-dirs ["resources/public/css"]             ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             }
+  :figwheel {:css-dirs ["resources/public/css"]}
 
   :hiera
   {:path          "ns-hierarchy.png"
