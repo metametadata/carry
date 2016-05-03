5beff10a5a16d50e881e2324c436d24ee12108a3
metametadata <yuri.govor@gmail.com>
2016-05-03 22:23:55 +0300

    prepare project for publishing

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject carry "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.51"]]
+  :description "ClojureScript application framework."
+  :url "https://github.com/metametadata/carry"
+  :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.8.51" :scope "provided"]]
 
   :pedantic? :abort
+
+  :source-paths ["src"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "resources/private" "target"])
