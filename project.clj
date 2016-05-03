4462df403e63877af433adfd01e404a97d8e26ad
metametadata <yuri.govor@gmail.com>
2016-05-03 23:05:10 +0300

    bump to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject carry "0.1.0-SNAPSHOT"
+(defproject carry "0.1.0"
   :description "ClojureScript application framework."
   :url "https://github.com/metametadata/carry"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
