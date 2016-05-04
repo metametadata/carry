0e53fb4d85caecfb744fcfd0ccc70a2d6b692750
metametadata <yuri.govor@gmail.com>
2016-05-04 22:39:01 +0300

    carry-schema: fix project url

--- contrib/schema/project.clj
+++ contrib/schema/project.clj
@@ -1,7 +1,7 @@
 (defproject
   carry-schema "0.1.0"
   :description "Carry middleware for model validation using Schema."
-  :url "https://github.com/metametadata/carry/tree/master/contrib/history"
+  :url "https://github.com/metametadata/carry/tree/master/contrib/schema"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
