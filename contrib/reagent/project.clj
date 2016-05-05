8629973a3b73ab98b76a5ebe50005e23eba1edb3
metametadata <yuri.govor@gmail.com>
2016-05-05 11:45:26 +0300

    carry-reagent: remove provided scope from some deps, don't require react-with-addons, use newest alpha2 version of Reagent

--- contrib/reagent/project.clj
+++ contrib/reagent/project.clj
@@ -7,10 +7,9 @@
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.8.51" :scope "provided"]
 
-                 [carry "0.1.0" :scope "provided"]
+                 [carry "0.1.0"]
 
-                 [reagent "0.6.0-alpha" :exclusions [cljsjs/react] :scope "provided"]
-                 [cljsjs/react-with-addons "15.0.2-0" :scope "provided"]]
+                 [reagent "0.6.0-alpha2"]]
 
   :pedantic? :abort
 
