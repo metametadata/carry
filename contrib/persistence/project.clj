0723e4729ee4f07ebef1750e89cc073e925e7551
metametadata <yuri.govor@gmail.com>
2016-05-05 11:43:09 +0300

    persistence: remove provided scope from some deps

--- contrib/persistence/project.clj
+++ contrib/persistence/project.clj
@@ -1,5 +1,5 @@
 (defproject
-  carry-persistence "0.2.0"
+  carry-persistence "0.3.0"
   :description "Carry middleware for automatic model saving/loading using browser storage."
   :url "https://github.com/metametadata/carry/tree/master/contrib/persistence"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.8.51" :scope "provided"]
 
-                 [org.clojure/core.match "0.3.0-alpha4" :scope "provided"]]
+                 [org.clojure/core.match "0.3.0-alpha4"]]
 
   :pedantic? :abort
 
