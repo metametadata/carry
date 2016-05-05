2db5577cf61bcf5529ad430e1ee2a2d77898c8ec
metametadata <yuri.govor@gmail.com>
2016-05-05 11:21:16 +0300

    carry-debugger: add readme and project files

---
+++ contrib/debugger/README.md
@@ -0,0 +1,5 @@
+# carry-debugger
+
+Middleware for debugging Carry applications.
+
+[![Clojars Project](https://img.shields.io/clojars/v/carry-debugger.svg)](https://clojars.org/carry-debugger)
---
+++ contrib/debugger/project.clj
@@ -0,0 +1,25 @@
+(defproject
+  carry-debugger "0.1.0"
+  :description "Middleware for debugging Carry applications."
+  :url "https://github.com/metametadata/carry/tree/master/contrib/debugger"
+  :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.8.51" :scope "provided"]
+
+                 [carry-schema "0.1.0"]
+                 [carry-reagent "0.1.0"]
+                 [reagent "0.6.0-alpha" :exclusions [cljsjs/react]]
+                 [cljsjs/react-with-addons "15.0.2-0"]
+
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [prismatic/schema "1.1.0"]
+                 [com.rpl/specter "0.10.0"]
+                 [cljsjs/jquery-ui "1.11.4-0"]
+                 [cljsjs/filesaverjs "1.1.20151003-0"]]
+
+  :pedantic? :abort
+
+  :source-paths ["src"]
+
+  :repositories {"clojars" {:sign-releases false}})
