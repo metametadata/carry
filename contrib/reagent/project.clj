8fe6b50a20fcf368c7cdbca29528766e6e99783c
metametadata <yuri.govor@gmail.com>
2016-05-13 14:53:25 +0300

    carry-reagant: bump Carry dep

--- contrib/reagent/project.clj
+++ contrib/reagent/project.clj
@@ -1,5 +1,5 @@
 (defproject
-  carry-reagent "0.2.0"
+  carry-reagent "0.3.0"
   :description "Reagent bindings for Carry."
   :url "https://github.com/metametadata/carry/tree/master/contrib/reagent"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.8.51" :scope "provided"]
 
-                 [carry "0.1.0"]
+                 [carry "0.2.0"]
 
                  [reagent "0.6.0-alpha2"]]
 
