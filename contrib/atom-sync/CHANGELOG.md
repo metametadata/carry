0333d062c029f9b194c65475039898c3107d4f22
metametadata <yuri.govor@gmail.com>
2016-06-30 22:48:24 +0300

    carry-atom-sync: bump to v0.2.0, add changelog, make it automatically sync from model for the first time

---
+++ contrib/atom-sync/CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## 0.2.0
+
+- Automatically resets synced atom after wrapped app is started so that user doesn't have to do it manually.
+- API docs clean up.
+
+## 0.1.0
+
+- Public release.
--- contrib/atom-sync/src/carry_atom_sync/core.cljs
+++ contrib/atom-sync/src/carry_atom_sync/core.cljs
@@ -1,31 +1,32 @@
 (ns carry-atom-sync.core
   (:require [cljs.core.match :refer-macros [match]]))
 
-(defn ^:no-doc -connect-atom
+(defn ^:no-doc -start-syncing
   [r model dispatch-signal]
-  ; sync model from reference
+  ; syncing model from reference
   (add-watch r
              model
              (fn [_key _ref _old-state new-state]
                (when (not= @model new-state)
                  (dispatch-signal [::on-reset new-state]))))
 
-  ; sync reference from model
+  ; syncing reference from model
   (add-watch model
              r
              (fn [_key _ref _old-state new-state]
                (when (not= @r new-state)
                  (reset! r new-state)))))
 
-(defn ^:no-doc -unconnect-atom
+(defn ^:no-doc -stop-syncing
   [r model]
   (remove-watch r model)
   (remove-watch model r))
 
 (defn add
-  "Carry middleware which on app start creates a bidirectional sync between the specified atom-like reference and the app model.
-   Sync is removed on app stop signal.
-   Synced references are not updated until one of the references changes."
+  "On :on-start signal, after wrapped app is started, creates a bidirectional sync between the specified atom-like reference
+   and the app model and also updates the reference from the model for the first time.
+   The initial reference value is not used.
+   Sync is removed on :on-stop signal."
   [spec r]
   (-> spec
       (update :control
@@ -37,11 +38,12 @@
                          (do
                            (app-control model signal dispatch-signal dispatch-action)
 
-                           (-connect-atom r model dispatch-signal))
+                           (reset! r @model)
+                           (-start-syncing r model dispatch-signal))
 
                          :on-stop
                          (do
-                           (-unconnect-atom r model)
+                           (-stop-syncing r model)
 
                            (app-control model signal dispatch-signal dispatch-action))
 
